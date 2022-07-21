fc8e1b1a584a3d1faed83024962327e16933d11c
Danish Pinjari <danishpinjari40@gmail.com>
2022-07-21 10:21:01 +0530

    Day-2

--- scripts/slider.js
+++ scripts/slider.js
@@ -3,15 +3,33 @@
 
 const img = document.querySelectorAll("img");
 
-let i = 0;
+// const btn = document.getElementsByClassName("btn_1");
+// let i = 0;
+// let count = 1;
+// setInterval( () => {
+//     img.forEach((img, i) => {
+//         img.style.display = 'none';
+        
+//     });
+//     if(i == img.length){
+//         i=0;
+//     }
+//     (btn+count).checked = true
+//         count++;
+//     if(count > 2){
+//         count = 1;
+//     }
+//     img[i].style.display = 'block';
+//     i++;
+// }, 2000);
 
-setInterval( () => {
-    img.forEach((img, i) => {
-        img.style.display = 'none';
-    });
-    if(i == img.length){
-        i=0;
+// btn_1
+let count = 1;
+setInterval(() => {
+    document.getElementsByClassName("btn_1"+count).checked = true;
+    count++;
+    if(count > 2){
+        count = 1;
     }
-    img[i].style.display = 'block';
-    i++;
 }, 2000);
+
