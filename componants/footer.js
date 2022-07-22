82f41f3491da0a9fd5f31ec1fff14d9b989b0f59
aakash indoriya <aakashindoriya12346@gmail.com>
2022-07-22 18:05:05 +0530

    day4 tasks complete

---
+++ componants/footer.js
@@ -0,0 +1,60 @@
+function footer(){
+    return `   <div id="Nikfooter">
+    <div id="footerd1">
+        <img id="footerd1I" src="https://static.cure.fit/assets/images/cult-logo-white.svg" alt="">
+        <p id="footerd1P">At cult.fit, we make group workouts fun, daily food healthy & tasty, mental fitness easy
+            with yoga & meditation, and
+            medical & lifestyle care hassle-free. #BeBetterEveryDay</p>
+    </div>
+    <div id="footerd2">
+        <p style="margin-top: 10%;" class="nikContact">CONTACT US</p>
+        <p class="nikContact">BLOG</p>
+        <p class="nikContact">PARTNER.FIT</p>
+        <p class="nikContact">TALKS</p>
+        <P class="nikContact">CAREERS</P>
+    </div>
+    <div id="footerd3">
+        <p class="nikContact1">CULTFIT FOR BUSINESSES</p>
+        <p class="nikContact1">CULT FRANCHISE</p>
+        <p class="nikContact1">CULT PASS NETWORK</p>
+        <P class="nikContact1">CORPORATE PARTNERSHIPS</P>
+    </div>
+    <div id="footerd4"></div>
+
+    <!-- app store part -->
+    <div id="footerd4">
+        <a href="https://apps.apple.com/us/app/cure-fit/id1217794588">
+            <img style="margin-left: 25%;margin-top: 20%;" src="https://static.cure.fit/assets/images/app-store.svg"
+                alt="">
+        </a>
+        <a href="https://play.google.com/store/apps/details?id=fit.cure.android">
+            <img style="margin-left: 25%;margin-top: 10%;"
+                src="https://static.cure.fit/assets/images/play-store.svg" alt="">
+        </a>
+    </div>
+</div>
+<div style="display: flex;color: whitesmoke;margin-top: -50px;" id="smallContact">
+    <p style="margin-left: 5%;">CONTACT US</p>
+    <p style="margin-left: 5%;">FAQ</p>
+    <p style="margin-left: 5%;">Terms & conditions</p>
+    <p style="margin-left: 5%;">Privacy Policy</p>
+    <p style="margin-left: 5%;">Refund Policy</p>
+    <a href="https://www.facebook.com/cultfitofficial">
+        <img style="margin-left: 300px;margin-top: 20px;"
+            src="https://static.cure.fit/assets/images/facebook_icon_white.svg" alt="">
+    </a>
+    <a href="https://twitter.com/cultfitOfficial">
+        <img style="margin-left: 50px;margin-top: 20px;"
+            src="https://static.cure.fit/assets/images/twitter_icon_white.svg" alt="">
+    </a>
+    <a href="https://www.instagram.com/cultfitOfficial/">
+        <img style="margin-left: 50px;margin-top: 20px;"
+            src="https://static.cure.fit/assets/images/instagram_icon_white.svg" alt="">
+    </a>
+    <a href="https://www.youtube.com/channel/UCSE72IaHOL-1Tv-m3JHE4Cg">
+        <img style="margin-left: 50px;margin-top: 20px;"
+            src="https://static.cure.fit/assets/images/youtube_icon_white_1.svg" alt="">
+    </a>
+</div>`
+}
+export {footer}
